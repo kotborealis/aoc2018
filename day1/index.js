5307231b8b37e7ac6ff2f9db19a3fce64a0baeff
kotborealis <kotborealis@gmail.com>
2018-12-01 18:23:48 +0300

    Day 1 part 1

---
+++ day1/index.js
@@ -0,0 +1,8 @@
+const sum = (accumulator, current) => accumulator + current;
+
+const data = require('fs')
+    .readFileSync(0, "utf-8")
+    .split(/[\n\r]+/)
+    .map(Number)
+    .reduce(sum, 0);
+console.log(data);
